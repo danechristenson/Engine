cdca83eb1bd9eee047f2c0c447e129f4683a2ec4
Dane Christenson <danechristenson@gmail.com>
2016-12-22 23:44:19 -0700

    added  beginnings following lazy foo, will expand outwards. build file still broken

--- build.bat
+++ build.bat
@@ -2,5 +2,5 @@
 
 mkdir ..\..\build
 pushd ..\..\build
-cl -FC -Zi ..\Engine\code\Engine.c
+cl -FC -Zi /I W:\lib\SDL2-2.0.5\lib\x64\SDL2 ..\Engine\code\Engine.c   /link SDL2.lib SDL.lib /SUBSYSTEM:CONSOLE
 popd
--- code/engine.c
+++ code/engine.c
@@ -1,6 +1,53 @@
+#include "SDL.h"
 #include <stdio.h>
 
-int main()
+
+ const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+
+int main (int argc, char* args[])
 {
-    return 0;
+    //window to render to
+    SDL_Wiindow* window = NULL;
+    
+    // surface within the window
+    SDL_Surface* screenSurface = NULL;
+    
+    // Initialize
+    if(SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        printf("SDL could not be initialized SDL_ERROR: %s\n", SDL_GetError());
+}
+else
+{ 
+    // Create window
+    window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if(window == NULL)
+    {
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+    }
+    else
+    {
+        // Get window surface
+        screenSurface = SDL_GetWindowSurface(window);
+        
+        // Fill surface white
+        SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+        
+        // Update the window
+        SDL_UpdateWindowsSurface(window);
+        
+        // Wait 2 seconds
+        SDL_Delay(2000);
+    }
+}
+
+// Destroy window
+SDL_DestroyWindow(window);
+
+// Quit SDL subsystems
+SDL_QUIT();
+
+return 0;
 }
