68acf7d3fe42f5b488e0a66462d12242498df4ca
Dane Christenson <danechristenson@gmail.com>
2016-12-24 13:58:38 -0700

    Changed the build file. still issues

---
+++ build - Copy.bat
@@ -0,0 +1,9 @@
+@echo off
+mkdir ..\engine\build
+pushd ..\engine\build
+cl -FC -Zi -nologo  ..\..\Engine\code\engine.c -IW:\lib\SDL2-2.0.5\include -link SDL2.lib SDL2main.lib -LIBPATH:W:\lib\SDL2-2.0.5\lib\x64 -SUBSYSTEM:CONSOLE
+popd
+
+
+
+
--- build.bat
+++ build.bat
@@ -3,18 +3,18 @@
 IF NOT EXIST ..\engine\build mkdir ..\engine\build
 pushd ..\engine\build
 
-del *pdb > NUL 2> NUL
-
-set PathMain=..\Engine\code\engine.c
-set Library=W:\lib\SDL2-2.0.5\lib\x64
+set MAIN_PATH=..\..\Engine\code\engine.c
+set LIBRARYPATH=W:\lib\SDL2-2.0.5\lib\x64
+set LIBRARIES=SDL2.lib SDL2main.lib
+set INCLUDE=W:\lib\SDL2-2.0.5\include
 
 set CompilerFlags=-FC -Zi -nologo
 
-cl 	%CompilerFlags% ^ 
-	%PathMain% ^
-	-IW:\lib\SDL2-2.0.5\include ^
-	-link SDL2.lib SDL2main.lib ^
-	-LIBPATH:%Library% ^
+cl %CompilerFlags% ^ 
+	%MAIN_PATH% ^
+	-I%INCLUDE% ^
+	-link %LIBRARIES% ^
+	-LIBPATH:%LIBRARYPATH% ^
 	-SUBSYSTEM:CONSOLE
-	
+		
 popd
